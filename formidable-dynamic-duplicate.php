04920cd3df92385d4d265fc5a19f2e3d4938bfa3
henrisusanto <henri@member.id>
2023-07-28 05:31:56 +0700

    init

---
+++ formidable-dynamic-duplicate.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * FF Dynamic Duplicate
+ *
+ * @package     DynamicDuplicate
+ * @author      Henri Susanto
+ * @copyright   2022 Henri Susanto
+ * @license     GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name: Dynamic Duplicate
+ * Plugin URI:  https://github.com/susantohenri/formidable-dynamic-duplicate
+ * Description: Prevent Distinct in FF Dynamic Field
+ * Version:     1.0.0
+ * Author:      Henri Susanto
+ * Author URI:  https://github.com/susantohenri/
+ * Text Domain: DynamicDuplicate
+ * License:     GPL v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
